3b37a22eb783e8a9a3ad513da8bca3f89c782458
Szymon <szymek0705@gmail.com>
2023-05-16 10:22:10 +0200

    poprawione dziala

--- QuizApp/models.py
+++ QuizApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class User(models.Model):
-    userName = models.CharField(max_length=100)
+    userName = models.CharField(max_length=100, blank=False, null=False)
     email = models.EmailField
     name = models.CharField(max_length=100)
     surName = models.CharField(max_length=100)
@@ -11,19 +11,21 @@ class User(models.Model):
 
 class Question(models.Model):
     tekst = models.CharField(max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     shuffleAnswers = models.BooleanField(default=True)
 
 class Answer(models.Model):
     tekst = models.CharField(max_length=200)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
-class questionAnswers(models.Model):
+class questionAnswer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
     answerOrder = models.IntegerField
-    isCorrect = models.BooleanField
+    isCorrect = models.BooleanField(default=False)
 
 class Quiz(models.Model):
+    class Meta:
+        verbose_name_plural = "quizes"
     question = models.ManyToManyField(Question)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
